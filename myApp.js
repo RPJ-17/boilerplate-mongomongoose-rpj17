a1435caeb0dbba7eabefcc6e59c0683441b9deee
juan rebolledo <juanjoserebolledo18@gmail.com>
2022-07-10 19:38:41 -0400

    ...

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 let validator = require('validator')
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URI);
 
 const personSchema = mongoose.Schema({
   name: {
