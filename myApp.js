1112cf03071646817d18b391cb8057ade02c8e04
juan rebolledo <juanjoserebolledo18@gmail.com>
2022-07-10 23:18:49 -0400

    ...

--- myApp.js
+++ myApp.js
@@ -94,14 +94,16 @@ const queryChain = (done) => {
   const foodToSearch = "burrito";
   Person
     .find({favoriteFoods: foodToSearch})
-    .sort({name: -1})
+    .sort({name: 1})
     .limit(2)
     .select({age: 0})
-    .exec((err, doc) => {
+    .exec((err, data) => {
       if (err) return console.log(err);
-      done(null, doc);
+      done(null, data);
     });
 };
+
+queryChain();
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
