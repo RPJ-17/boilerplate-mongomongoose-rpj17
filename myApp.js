c6c9d3ebc86d1803dc9977ffd037a6928563b99b
juan rebolledo <juanjoserebolledo18@gmail.com>
2022-07-10 22:28:11 -0400

    ...

--- myApp.js
+++ myApp.js
@@ -70,13 +70,16 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
   Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, updatedDoc) => {
-    if(err) return console.log(err);
+    if (err) return console.log(err);
     done(null, updatedDoc);
   });
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, removedDoc) => {
+    if (err) return console.log(err);
+    done(null, removedDoc);
+  });
 };
 
 const removeManyPeople = (done) => {
