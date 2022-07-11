f7656cc7a57783894f26554958012694d4a5fde8
juan rebolledo <juanjoserebolledo18@gmail.com>
2022-07-10 22:50:21 -0400

    ...

--- myApp.js
+++ myApp.js
@@ -92,8 +92,15 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person
+    .find({favoriteFoods: foodToSearch})
+    .sort({name: -1})
+    .limit(2)
+    .select({age: 0})
+    .exec((err, doc) => {
+      if (err) return console.log(err);
+      done(null, doc);
+    });
 };
 
 /** **Well Done !!**
