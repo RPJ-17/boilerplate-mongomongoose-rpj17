51cdc2fd96136b807c84330644a847f2985d7f2c
juan rebolledo <juanjoserebolledo18@gmail.com>
2022-07-10 20:05:17 -0400

    ...

--- myApp.js
+++ myApp.js
@@ -50,11 +50,13 @@ const findPeopleByName = (personName, done) => {
     if (err) return console.log(err);
     done(null, personFound);
   });
-  
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food}, (err, data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  })
 };
 
 const findPersonById = (personId, done) => {
